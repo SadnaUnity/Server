4edd679faa75359e50e3f6dd1a1a97c5f8068d38
Mai Shani <mai.shani@sap.com>
2023-04-01 13:08:00 +0300

    [INFRA] cretae db connection

--- src/main/java/com/example/server/controllers/LoginController.java
+++ src/main/java/com/example/server/controllers/LoginController.java
@@ -1,53 +1,69 @@
 package com.example.server.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.sql.*;
 
 @RestController
 public class LoginController {
 
-    private String jdbcUrl;
+    Connection connectionDB;
     private String databaseName;
-    private String instanceConnectionName;
     private String dbUsername;
     private String dbPassword;
+    private String dbHostAddress;
+    private String dbHostPort;
 
-    public LoginController()
-    {
+    public LoginController() {
+//        instanceConnectionName = "sadna-db-server";
         databaseName = "sadnaDB";
-        instanceConnectionName = "sadna-db-server";
         dbUsername = "root";
         dbPassword = "Chxkhcmk69";
+        dbHostAddress = "34.165.31.251";
+        dbHostPort = "3306";
+        String jdbcUrl = "jdbc:mysql://" + dbHostAddress + ":" + dbHostPort + "/" + databaseName;
 
-        jdbcUrl = String.format(
-                "jdbc:mysql://34.165.31.251:3306/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory",
-                databaseName, instanceConnectionName);
-    }
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        if(checkUserExists(username, password))
-            return "GOOD";
-        else
-            return "bad";
-
-    }
-    public boolean checkUserExists(String username, String password) {
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword)) {
-            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, username);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-            return resultSet.next();
+//        jdbcUrl = String.format(
+//                "jdbc:mysql://34.165.31.251:3306/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory",
+//                databaseName, instanceConnectionName);
+        try {
+            connectionDB = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
+            //TODO: connectionDB.close();
 
         } catch (SQLException e) {
-            System.err.println("Failed to check if user exists: " + e.getMessage());
-            return false;
+            //TODO: handle exception
+            throw new RuntimeException(e);
         }
     }
 
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        if (checkValidUser(username, password))
+            return "valid";
+        else
+            return "invalid";
+    }
+
+    public boolean checkValidUser(String username, String password) {
+        boolean validUser = false;
+        try {
+            String sql = "SELECT * FROM users"; //Prepare the SQL statement
+            Statement stmt = connectionDB.createStatement();
+            ResultSet dbResponse = stmt.executeQuery(sql); // Execute the SQL statement and get the results
+
+            while (dbResponse.next()) {
+                if (dbResponse.getString("username").equals(username)) {
+                    if (dbResponse.getString("password").equals(password))
+                        validUser = true;
+                }
+            }
+
+            dbResponse.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return validUser;
+    }
 }
+
